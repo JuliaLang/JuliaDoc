a4c7dcc80eb7101a25eacc60229e70ae78b49d9c
Patrick O'Leary <patrick.oleary@gmail.com>
2013-05-27 15:17:59 -0500

    Added function to find templates directory

--- juliadoc/__init__.py
+++ juliadoc/__init__.py
@@ -9,13 +9,22 @@ def get_theme_dir():
     """
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "theme"))
 
+def get_templates_dir():
+    """
+    Returns path to directory containing this package's templates.
+    
+    This is designed to be used when setting the ``templates_path``
+    option within Sphinx's ``conf.py`` file.
+    """
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
+
 def default_sidebars():
     """
     Returns a dictionary mapping for the templates used to render the
     sidebar on the index page and sub-pages.
     """
     return {
-        '**': ['localtoc.html', 'relations.html', 'searchbox.html'],
+        '**': ['localtoc.html', 'searchbox.html'],
         'index': [],
         'search': [],
     }
