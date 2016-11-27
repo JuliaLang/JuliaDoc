00ed19c6dc7c44cfd6fd9de99f38ccfdc71f3a6c
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-27 13:44:26 +0100

    Add theme files to package_data
    This fixes #2.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='JuliaDoc',
     version='0.0.0',
-    packages=['juliadoc',],
+    packages=['juliadoc'],
+    package_data={'juliadoc': ['theme/julia/*', 'theme/julia/static/*']},
     license='MIT',
     long_description=open('README.md').read(),
 )
